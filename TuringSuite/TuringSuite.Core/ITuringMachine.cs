47332ef5d2580ae8c31bf1419bfb85a484b423c4
Ben Burns <benjaminaburns@gmail.com>
2020-10-31 23:26:27 -0500

    initital commit

---
+++ TuringSuite/TuringSuite.Core/ITuringMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TuringSuite.Core
+{
+    public interface ITuringMachine
+    {
+        List<int> States { get; set; }
+
+        List<int> AlphabetSymbols { get; set; }
+
+        int BlankSymbol { get; set; }
+
+        int InitialState { get; set; }
+
+        List<int> HaltingStates { get; set; }
+
+        int HeadPosition { get; }
+
+        int CurrentState { get; }
+
+        void TransitionFunction();
+    }
+}
