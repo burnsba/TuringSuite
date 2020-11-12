0dd77271aa04debe548e235edabfe96c15fdca86
Ben Burns <benjaminaburns@gmail.com>
2020-11-11 21:14:18 -0600

    web: add json loader, improve ui

--- TuringSuite/TuringSuite.Web/Static/js/tm/Bb1d2s2s.js
+++ TuringSuite/TuringSuite.Web/Static/js/tm/Bb1d2s2s.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const Bb1d2s2s = {
+	"Author": "",
+	"Date": "",
+	"Version": "",
+	"Comment": "",
+	"Description": "2-state, 2-symbol busy beaver",
+	"Reference": "<a href=\"https://en.wikipedia.org/wiki/Busy_beaver\">https://en.wikipedia.org/wiki/Busy_beaver</a>",
+	
 	"TapeType": "1DimensionalNonFinite",
 	"NonHaltingStates": ["A", "B"],
 	"HaltingStates": ["HALT"],
@@ -12,28 +19,28 @@ const Bb1d2s2s = {
 			"FromSymbol": "0",
 			"MoveOffsetX": 1,
 			"NextState": "B",
-			"WriteSymbol": "1",
+			"WriteSymbol": "1"
 		},
 		{
 			"FromState": "A",
 			"FromSymbol": "1",
 			"MoveOffsetX": -1,
 			"NextState": "B",
-			"WriteSymbol": "1",
+			"WriteSymbol": "1"
 		},
 		{
 			"FromState": "B",
 			"FromSymbol": "0",
 			"MoveOffsetX": -1,
 			"NextState": "A",
-			"WriteSymbol": "1",
+			"WriteSymbol": "1"
 		},
 		{
 			"FromState": "B",
 			"FromSymbol": "1",
 			"MoveOffsetX": 1,
 			"NextState": "HALT",
-			"WriteSymbol": "1",
+			"WriteSymbol": "1"
 		}
 	]
 };
--- TuringSuite/TuringSuite.Web/Static/js/tm/hmabu90_no5.js
+++ TuringSuite/TuringSuite.Web/Static/js/tm/hmabu90_no5.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const Bb1d4s2s = {
-	"Author": "",
-	"Date": "",
+const Hmabu90_no5 = {
+	"Author": "Heiner Marxen",
+	"Date": "1990",
 	"Version": "",
-	"Comment": "",
-	"Description": "4-state, 2-symbol busy beaver",
-	"Reference": "<a href=\"https://en.wikipedia.org/wiki/Busy_beaver\">https://en.wikipedia.org/wiki/Busy_beaver</a>",
+	"Comment": "simple counter",
+	"Description": "TM #5 from MaBu90-Paper",
+	"Reference": "<a href=\"http://turbotm.de/~heiner/BB/simmbP_5.html\">http://turbotm.de/~heiner/BB/simmbP_5.html</a>",
 	
 	"TapeType": "1DimensionalNonFinite",
-	"NonHaltingStates": ["A", "B", "C", "D"],
+	"NonHaltingStates": ["A", "B", "C", "D", "E"],
 	"HaltingStates": ["HALT"],
 	"InitialState": "A",
 	
@@ -17,23 +17,23 @@ const Bb1d4s2s = {
 		{
 			"FromState": "A",
 			"FromSymbol": "0",
-			"MoveOffsetX": 1,
+			"MoveOffsetX": -1,
 			"NextState": "B",
 			"WriteSymbol": "1"
 		},
 		{
 			"FromState": "A",
 			"FromSymbol": "1",
-			"MoveOffsetX": -1,
-			"NextState": "B",
+			"MoveOffsetX": 1,
+			"NextState": "A",
 			"WriteSymbol": "1"
 		},
 		{
 			"FromState": "B",
 			"FromSymbol": "0",
-			"MoveOffsetX": -1,
+			"MoveOffsetX": 1,
 			"NextState": "A",
-			"WriteSymbol": "1"
+			"WriteSymbol": "0"
 		},
 		{
 			"FromState": "B",
@@ -46,8 +46,8 @@ const Bb1d4s2s = {
 			"FromState": "C",
 			"FromSymbol": "0",
 			"MoveOffsetX": 1,
-			"NextState": "HALT",
-			"WriteSymbol": "1"
+			"NextState": "A",
+			"WriteSymbol": "0"
 		},
 		{
 			"FromState": "C",
@@ -59,16 +59,30 @@ const Bb1d4s2s = {
 		{
 			"FromState": "D",
 			"FromSymbol": "0",
-			"MoveOffsetX": 1,
-			"NextState": "D",
-			"WriteSymbol": "1"
+			"MoveOffsetX": -1,
+			"NextState": "E",
+			"WriteSymbol": "0"
 		},
 		{
 			"FromState": "D",
 			"FromSymbol": "1",
 			"MoveOffsetX": 1,
-			"NextState": "A",
+			"NextState": "B",
+			"WriteSymbol": "1"
+		},
+		{
+			"FromState": "E",
+			"FromSymbol": "0",
+			"MoveOffsetX": 1,
+			"NextState": "B",
 			"WriteSymbol": "0"
 		},
+		{
+			"FromState": "E",
+			"FromSymbol": "1",
+			"MoveOffsetX": -1,
+			"NextState": "HALT",
+			"WriteSymbol": "1"
+		}
 	]
 };
